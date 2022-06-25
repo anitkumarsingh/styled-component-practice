4bbb44caf999523ef884efb5f1d14cd2878e3745
anitkumarsingh <anikumarsingh99@gmail.com>
2022-06-25 15:51:17 +0530

    overiding Non styled component

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,25 +1,50 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
 import styled from 'styled-components';
 
 const HeaderStyle = styled.header`
-	height: 40px;
-	padding: 0 16px;
-	background: #666;
-	font-size: 16px;
+	height: 60px;
 	width: 100%;
 	box-sizing: border-box;
 	display: flex;
-  position: fixed;
-  top: 0;
+	padding: 0 16px;
+	background: #eeee;
+	position: fixed;
+	top: 0;
 `;
 
+const Menus = styled.nav`
+	display: flex;
+	position: relative;
+	width: initial;
+	margin: auto 0 auto auto;
+	border-bottom: none;
+	font-family: 'Open-sans';
+	background: none;
+	left: initial;
+	top: initial;
+`;
+const Link = ({ isActive, children, ...props }) => {
+	return <RouterLink {...props}>{children}</RouterLink>;
+};
+
+const StyledLink = styled(Link)`
+	padding: 0 5px;
+	text-align: center;
+	text-decoration: none;
+	margin: auto 0;
+	font-weight: ${(p) => (p.isActive ? 'bold' : 'normal')};
+`;
 const Header = () => {
 	return (
 		<HeaderStyle>
-			<Link to='/'>Home</Link>
-			<Link to='/login'>Login</Link>
-			<Link to='/button'>Buttons</Link>
+			<Menus>
+				<StyledLink to='/'>Home</StyledLink>
+				<StyledLink to='/login' isActive>
+					Login
+				</StyledLink>
+				<StyledLink to='/button'>Buttons</StyledLink>
+			</Menus>
 		</HeaderStyle>
 	);
 };
--- src/components/common/PageLayout.js
+++ src/components/common/PageLayout.js
@@ -7,7 +7,7 @@ const Contents = styled.main`
 	margin: 80px auto 0 auto;
 	padding: 0 16px;
 	box-sizing: border-box;
-	font-family: 'Open Sans';
+	font-family: 'Open-Sans';
    h1, h2, h3, h4, h5, h6 {
 		font-family: 'Kaushan script';
 	}
