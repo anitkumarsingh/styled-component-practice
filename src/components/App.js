2ab193c9364dc41153fb5863c6e2e2e9ea19310a
anitkumarsingh <anikumarsingh99@gmail.com>
2022-06-25 13:21:00 +0530

    bulk styling with css helper

--- src/components/App.js
+++ src/components/App.js
@@ -18,6 +18,8 @@ const App = () => {
 			<h1>App</h1>
       <Button>Primary Button</Button>
 			<Button secondary>Secondary Button</Button>
+      <Button disabled>Disabled Button</Button>
+      <Button large secondary>Large Button</Button>
 		</>
 	);
 };
--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,19 +1,29 @@
-import styled from "styled-components";
+import styled, { css } from 'styled-components';
 
 const Button = styled.button`
- color: white;
- background:${props=>props.secondary ? 'black' :'#f8049c'};
- font-weight: bold;
- padding: 8px;
- border-radius: 4px;
- box-shadow: none;
- border: none;
- font-size: 1em;
- display: block;
- width: 100%;
- &:disabled{
-   color: #666;
-   background: #666;
- }
+	color: white;
+	background: ${(props) => (props.secondary ? 'black' : '#f8049c')};
+	font-weight: bold;
+	${(props) =>
+		props.large
+			? css`
+					padding: 10px;
+					border-radius: 5px;
+					font-size: 1.5em;
+			  `
+			: css`
+					padding: 8px;
+					border-radius: 4px;
+					font-size: 1em;
+			  `}
+	box-shadow: none;
+	border: none;
+
+	display: block;
+	width: 100%;
+	&:disabled {
+		color: #666;
+		background: #f6f6f6;
+	}
 `;
 export { Button };
