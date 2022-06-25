5e34aba508a34a9277d1f8739382d645179416bb
anitkumarsingh <anikumarsingh99@gmail.com>
2022-06-25 13:00:30 +0530

    global style

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
-import {Button} from "components/common";
+import { Button } from 'components/common';
+import { createGlobalStyle } from 'styled-components';
 
-const App = ()=> {
-  return (
-    <>
-      <h1>App</h1>
-      <Button>Test</Button>
-    </>
-  
-  );
+const GlobalStyle = createGlobalStyle`
+body{
+  background: white;
+  font-family: 'Kaushan Script';
+  min-height: 100vh;
+  color: black;
+  margin: 0;
 }
+`;
+
+const App = () => {
+	return (
+		<>
+			<GlobalStyle />
+			<h1>App</h1>
+			<Button>Test</Button>
+		</>
+	);
+};
 
 export default App;
