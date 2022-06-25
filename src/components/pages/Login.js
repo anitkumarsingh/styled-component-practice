aa22edee267cd58f96c68f3d86c10746433f749d
anitkumarsingh <anikumarsingh99@gmail.com>
2022-06-25 18:05:55 +0530

    form input field

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,10 +1,48 @@
-import { PageLayout } from 'components/common';
+import { useState } from 'react';
+import { PageLayout, Input } from 'components/common';
+import styled from 'styled-components';
+
+const Forms = styled.form`
+	width: 100%;
+	max-width: 400px;
+	background: white;
+	padding: 16px;
+	box-sizing: border-box;
+	border-radius: 4px;
+	border: 2px solid #eeee;
+	margin: auto 5px;
+`;
 
 const Login = () => {
+	const [formFields, setFormFields] = useState({ username: '', password: '' });
+	const handleChange = (e) => {
+		e.persist();
+		setFormFields((s) => ({
+			...s,
+			[e.target.name]: e.target.value
+		}));
+	};
+	
 	return (
 		<>
 			<PageLayout />
 			<h1>Login</h1>
+			<Forms>
+				<Input
+					onChange={handleChange}
+					value={formFields.username}
+					name='username'
+					type='text'
+					placeholder='Please enter username'
+				/>
+				<Input
+					onChange={handleChange}
+					value={formFields.password}
+					type='password'
+					name='password'
+					placeholder='Please enter password'
+				/>
+			</Forms>
 		</>
 	);
 };
